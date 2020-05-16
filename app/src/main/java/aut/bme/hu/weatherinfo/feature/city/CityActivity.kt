8f30234bb6491fcefff20798c748df3f9b49ec67
rolandfenyes <ftendalan@gmail.com>
2020-05-16 11:35:19 +0200

    CityAdapter class got new functions

--- app/src/main/java/aut/bme/hu/weatherinfo/feature/city/CityActivity.kt
+++ app/src/main/java/aut/bme/hu/weatherinfo/feature/city/CityActivity.kt
@@ -3,21 +3,50 @@ package aut.bme.hu.weatherinfo.feature.city
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import aut.bme.hu.weatherinfo.R
 
 import kotlinx.android.synthetic.main.activity_city.*
+import kotlinx.android.synthetic.main.content_city.*
 
-class CityActivity : AppCompatActivity() {
+class CityActivity : AppCompatActivity(), CityAdapter.OnCitySelectedListener, AddCityDialogFragment.AddCityDialogListener {
+
+    private lateinit var adapter: CityAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city)
         setSupportActionBar(toolbar)
 
+        initFab()
+        initRecyclerView()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun initRecyclerView() {
+        MainRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = CityAdapter(this)
+        adapter.addCity("Budapest")
+        adapter.addCity("Debrecen")
+        adapter.addCity("Sopron")
+        adapter.addCity("Szeged")
+        MainRecyclerView.adapter = adapter
+    }
+
+    private fun initFab() {
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCitySelected(city: String?) {
+        TODO("Not yet implemented")
     }
 
 }
---
+++ app/src/main/java/aut/bme/hu/weatherinfo/feature/city/CityAdapter.kt
@@ -0,0 +1,55 @@
+package aut.bme.hu.weatherinfo.feature.city
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import aut.bme.hu.weatherinfo.R
+
+class CityAdapter (private val listener: OnCitySelectedListener) : RecyclerView.Adapter<CityAdapter.CityViewHolder>() {
+
+    private var cities: MutableList<String> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityAdapter.CityViewHolder {
+        return CityViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_city, parent, false))
+    }
+
+    override fun getItemCount(): Int = cities.size
+
+    override fun onBindViewHolder(holder: CityAdapter.CityViewHolder, position: Int) {
+        val item = cities[position]
+        holder.bind(item)
+    }
+
+    fun addCity(newCity: String) {
+        cities.add(newCity)
+        notifyItemInserted(cities.size - 1)
+    }
+
+    fun removeCity(position: Int) {
+        cities.removeAt(position)
+        notifyItemRemoved(position)
+        if (position < cities.size) {
+            notifyItemRangeChanged(position, cities.size - position)
+        }
+    }
+
+    inner class CityViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var item: String? = null
+        init {
+            itemView.setOnClickListener {
+                listener.onCitySelected(item)
+            }
+        }
+        fun bind(newCity: String?) {
+            item = newCity
+            itemView.CityItemNameTextView.text = item
+        }
+    }
+
+    interface OnCitySelectedListener {
+        fun onCitySelected(city: String?)
+    }
+
+
+}
