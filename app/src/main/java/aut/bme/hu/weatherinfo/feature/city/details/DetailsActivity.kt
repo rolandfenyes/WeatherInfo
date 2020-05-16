553597254def750edf27ffff4ad0262810eb04f3
rolandfenyes <ftendalan@gmail.com>
2020-05-16 12:01:48 +0200

    DetailsActivity added

---
+++ app/src/main/java/aut/bme/hu/weatherinfo/feature/city/details/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package aut.bme.hu.weatherinfo.feature.city.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import aut.bme.hu.weatherinfo.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
